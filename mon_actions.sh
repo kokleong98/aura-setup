d580ae1c1f602c14cf85c6366b99786c28b1b09f
SOO KOK LEONG <kokleong98@gmail.com>
2019-02-08 18:01:44 +0800

    Update mon_actions.sh
    added mail notification on staking offline

--- mon_actions.sh
+++ mon_actions.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
+#sendmail required postfix service to be install. (sendmail=1 to make email notification)
+#
+sendmail=0
+mail_subject="AURA STAKING OFFLINE."
+mail_message="AURA STAKING OFFLINE."
+mail_to="Your@email.com"
+
 #check whether last 10 consecutives staking offline
 test=$(aura logs | grep STAKING | tail -n 10 | grep OFFLINE -c)
 if [ $test -eq 10 ]
 then
+  if [ sendmail -eq 1 ]
+  then
+    echo $mail_message | mail -s $mail_message $mail_to
+  fi
+
   #stop aura services
   aura stop
   #update aurad
