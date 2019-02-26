bb57fd036ee25cd0375fe1d2ce1cecb5714bbeed
SOO KOK LEONG <kokleong98@gmail.com>
2019-02-26 16:16:57 +0800

    Update configure_mail.sh

--- configure_mail.sh
+++ configure_mail.sh
@@ -35,14 +35,15 @@ fi
 debconf-set-selections <<< "postfix postfix/mailname string $(hostname)"
 debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
 sudo apt-get install postfix mailutils -y
-sasl_options="[$smtp_server]:$smtp_port $email:$password" 
+sasl_options="[${smtp_server}]:${smtp_port} ${email:$password}" 
+echo $sasl_options
 cat >> /etc/postfix/sasl/sasl_passwd <<< "$sasl_options" 
 sudo postmap /etc/postfix/sasl/sasl_passwd
 
 sudo chown root:root /etc/postfix/sasl/sasl_passwd /etc/postfix/sasl/sasl_passwd.db
 sudo chmod 0600 /etc/postfix/sasl/sasl_passwd /etc/postfix/sasl/sasl_passwd.db
 
-sudo postconf -e 'relayhost = [$smtp_server]:$smtp_port'
+sudo postconf -e 'relayhost = [${smtp_server}]:${smtp_port}'
 sudo postconf -e 'smtp_sasl_auth_enable = yes'
 sudo postconf -e 'smtp_sasl_security_options = noanonymous'
 sudo postconf -e 'smtp_sasl_password_maps = hash:/etc/postfix/sasl/sasl_passwd'
